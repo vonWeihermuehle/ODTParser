cfb550f3ed9c851ac21e740a7ca5c05d32dd816d
Max <max@mb-media.net>
2021-04-19 23:39:24 +0200

    add CLI

--- src/net/mbmedia/odtparser/Main.java
+++ src/net/mbmedia/odtparser/Main.java
@@ -1,15 +1,30 @@
 package net.mbmedia.odtparser;
 
-public class Main {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-    public static final String path_to_odt = "C:\\Users\\Max\\repo\\Bootsfreund\\src\\main\\webapp\\freizeit\\von_Saal_nach_Tulln.odt";
+public class Main {
 
     public Main() {
     }
 
-    public static void main(String[] args) {
-        ODTParser parser = new ODTParser(path_to_odt);
-        System.out.println(parser.transform());
+    public static void main(String[] args) throws IOException {
+        if(!checkInput(args)){
+            return;
+        }
+
+        ODTParser parser = new ODTParser(args[0]);
+        Files.write(Paths.get(args[1]), parser.transform().getBytes());
+    }
+
+    private static boolean checkInput(String[] args){
+        if(args.length != 2){
+            System.out.println("Usage: java -jar ODTParser.jar <path_to_input_file.odt> <output_file.html>");
+            return false;
+        }
+
+        return true;
     }
 
 
